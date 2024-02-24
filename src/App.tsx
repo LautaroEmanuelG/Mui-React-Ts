9dfaf87764e18aa902d7dc7ac42d98721eece4f2
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-02-24 12:17:23 -0300

    Inicio del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material"
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola gente</h1>
+      <Button variant="contained">Clikeame</Button>
+    </>
+  )
+}
+
+export default App
