5ede016a276d6221b9b57a70f03bdbc6fbe72ebe
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-02-25 18:29:08 -0300

    Some updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@mui/material"
+import { Button, Container, Typography } from '@mui/material';
 
 function App() {
-
-  return (
-    <>
-      <h1>Hola gente</h1>
-      <Button variant="contained">Clikeame</Button>
-    </>
-  )
+	return (
+		<Container
+			maxWidth="md"
+			sx={{ border: 2, boxShadow: 3 }}>
+			<h1>Hola gente</h1>
+			<Typography variant="h1">h1 con MUI</Typography>
+			<Button variant="contained">Clikeame</Button>
+		</Container>
+	);
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,24 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#ff5014',
+		},
+		secondary: {
+			main: '#219fdf',
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-    <CssBaseline/>
-		<App />
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
